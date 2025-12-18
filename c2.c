ae8aacaa8fbb294bf69112b72d29250cfc0a8bea
innocentx0 <abateke@gmail.com>
2025-12-18 21:33:43 +0100

    Changes..

--- c2.c
+++ c2.c
@@ -2,15 +2,41 @@
 #include <stdio.h>
 
 
+void isWindows() {
+    printf("Machine: windows\n");
+}
 
-int main(){
-    printf("Progam is starting..");
-    int returnCode = system("wget http://127.0.0.1:4242/a.txt -o a.txt");
 
-    if (returnCode == 0){
-        printf("File obtained successfully..");
-    }else{
-        printf("command execution failed.. aborting");
+
+void isLinux() {
+    printf("Machine: linux\n");
+    
+    int checkServer = system("ping -c 1 127.0.0.1 > /dev/null 2>&1");
+
+    if (checkServer == 0) {
+        printf("Program is starting..\n");
+        
+        int returnCode = system("wget -q http://127.0.0.1:4242/a.txt -O a.txt");
+
+        if (returnCode == 0) {
+            printf("File obtained successfully..\n");
+        } else {
+            printf("Command execution failed.. aborting\n");
+        }
+    } 
+    else {
+        printf("Server unreachable.\n");
     }
+
+}
+
+int main() {
+    #ifdef _WIN32
+        printf("Running in windows..");
+        isWindows();
+    #elif __linux__
+        printf("Running on linux..");
+        isLinux();
+    #endif
     return 0;
 }
